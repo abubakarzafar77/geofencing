20a999e300bd89555dcab56563e972463acea6b2
abubakarzafar77 <abubakar.zafar@purelogics.net>
2017-10-20 07:33:13 -0700

    add googlemap npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
+
+import { MapsAPILoader } from '@agm/core';
+import { } from 'googlemaps';
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	title: string = 'My first AGM project';
-	lat: number = 51.678418;
-	lng: number = 7.809007;
+
+    constructor(
+        private mapsAPILoader: MapsAPILoader,
+        private ngZone: NgZone,
+    ) { }
+    title: string = 'My first AGM project';
+    lat: number = 51.678418;
+    lng: number = 7.809007;
+    zoom: number = 8;
+
+
+    mapClicked($event) {
+        var totalMarker: number = this.markers.length;
+        // console.log(String.fromCharCode(65 + totalMarker));
+        var nextLabel: string = String.fromCharCode(65 + totalMarker)
+
+        console.log(this.markers.length);
+        this.markers.push({
+            lat: $event.coords.lat,
+            lng: $event.coords.lng,
+            label: nextLabel
+        })
+    }
+
+    clickedMarker(label: string, index: number) {
+        console.log(`clicked the marker: ${label}`)
+    }
+    // clickedCircle($event) {
+    //     console.log($event);
+    // }
+
+    markers: marker[] = [
+        {
+            lat: 51.673858,
+            lng: 7.815982,
+            label: 'A',
+            draggable: true
+        },
+        {
+            lat: 51.373858,
+            lng: 7.215982,
+            label: 'B',
+            draggable: false
+        },
+        {
+            lat: 51.723858,
+            lng: 7.895982,
+            label: 'C',
+            draggable: true
+        }
+    ]
 }
+
+// just an interface for type safety.
+interface marker {
+    lat: number;
+    lng: number;
+    label?: string;
+    draggable?: boolean;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,3 +25,5 @@ import { AppComponent } from './app.component';
     bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
